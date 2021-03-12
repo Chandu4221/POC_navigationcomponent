f3eb6e5739e2f5adc98679b6332a990ebc6b4e74
Chandu4221 <chndraja@gmail.com>
2021-03-12 20:55:41 +0530

    bottom action bar added with fab

--- app/src/main/java/com/example/demoApp/MainActivity.kt
+++ app/src/main/java/com/example/demoApp/MainActivity.kt
@@ -1,6 +1,6 @@
+package com.example.demoApp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.demoApp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
